aaea7804199767d9ad29652eb048aafd32a0cf88
quinn <q.shanahan@gmail.com>
2023-12-19 22:56:03 -0500

    fmt

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,16 @@
-import { type PageProps } from "$fresh/server.ts";
+import { type PageProps } from '$fresh/server.ts'
 export default function App({ Component }: PageProps) {
-  return (
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>mixtapes</title>
-        <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>
-        <Component />
-      </body>
-    </html>
-  );
+	return (
+		<html>
+			<head>
+				<meta charset='utf-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<title>5/3/1 Calculator</title>
+				<link rel='stylesheet' href='/styles.css' />
+			</head>
+			<body>
+				<Component />
+			</body>
+		</html>
+	)
 }
